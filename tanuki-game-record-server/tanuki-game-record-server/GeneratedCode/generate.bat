3b7ba37406a1f0e663f9c881a4dc9b6620523732
Hisayori Noda <nodchip@gmail.com>
2017-02-19 12:03:03 +0900

    ソリューションを追加した grpcサービス定義を追加した

---
+++ tanuki-game-record-server/tanuki-game-record-server/GeneratedCode/generate.bat
@@ -0,0 +1,1 @@
+..\..\packages\Grpc.Tools.1.1.0\tools\windows_x64\protoc.exe --csharp_out . --grpc_out . TanukiGameRecordServer.proto --plugin=protoc-gen-grpc=..\..\packages\Grpc.Tools.1.1.0\tools\windows_x64\grpc_csharp_plugin.exe
---
+++ tanuki-game-record-server/tanuki-game-record-server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tanuki_game_record_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
